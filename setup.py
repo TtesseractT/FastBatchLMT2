74b040649c29b4053b9b1235f9d4c471c4c1cfd3
Sabian Hibbs <Sabian.hibbs@gmail.com>
2024-05-17 05:45:16 +0100

    xc

--- setup.py
+++ setup.py
@@ -6,28 +6,27 @@
 # Version: 1.0.1                                                    #
 #-------------------------------------------------------------------#
 
-
 import os
 import subprocess
 
 try:
     print('Checking Deps')
-    subprocess.run('whisper', '--version', shell=True)
+    subprocess.run(['whisper', '--version'], shell=True)
 except:
-    subprocess.run('pip', 'install', 'git+https://github.com/openai/whisper.git')
-    subprocess.run('pip', 'install', '--upgrade', '--no-deps', '--force-reinstall', 'git+https://github.com/openai/whisper.git')
-    subprocess.run('pip', 'install', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu118')
+    subprocess.run(['pip', 'install', 'git+https://github.com/openai/whisper.git'])
+    subprocess.run(['pip', 'install', '--upgrade', '--no-deps', '--force-reinstall', 'git+https://github.com/openai/whisper.git'])
+    subprocess.run(['pip', 'install', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu118'])
 
 # Check if Pipx is installed.
 try:
     print("Checking pipx Install")
-    subprocess.run('python', 'pipx', '--version')
+    subprocess.run(['python', 'pipx', '--version'])
 except:
     print("pipx not installed, installing")
-    subprocess.run('pip', 'install', 'pipx')
+    subprocess.run(['pip', 'install', 'pipx'])
 
-subprocess.run(f'pipx install insanely-fast-whisper', shell=True)
-subprocess.run(f'pipx install insanely-fast-whisper --force --pip-args="--ignore-requires-python"', shell=True)
+subprocess.run(['pipx', 'install', 'insanely-fast-whisper'], shell=True)
+subprocess.run(['pipx', 'install', 'insanely-fast-whisper', '--force', '--pip-args="--ignore-requires-python"'], shell=True)
 
 # Create the directories if they don't exist
 if not os.path.exists('Input-Videos'):

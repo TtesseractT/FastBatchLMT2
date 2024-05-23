ac2f8e8c14b4d8db761ee3a3a17c8f6ed332cbb3
Sabian Hibbs <Sabian.hibbs@gmail.com>
2024-05-23 15:48:25 +0100

    xc

--- server.py
+++ server.py
@@ -182,5 +182,5 @@ iface = gr.Interface(
 
 if __name__ == "__main__":
     #iface.launch(server_name="0.0.0.0", server_port=7860, share=False)
-    iface.launch(server_name="0.0.0.0", server_port=80, share=False)
+    iface.launch(server_name="0.0.0.0", server_port=8080, share=False)
 
